531cd0b51295067cbd327ce938be32cf151e050b
Allysson <victorkratos333@gmail.com>
2023-11-01 12:28:21 -0300

    123

---
+++ JS/index.js
@@ -0,0 +1,13 @@
+let ul = document.querySelector('nav .ul')
+
+function openMenu() {
+    ul.classList.add('open');
+}
+function closeMenu() {
+    ul.classList.remove('open');
+}
+const chk = document.getElementById('chk')
+
+chk.addEventListener('change',() => {
+    document.body.classList.toggle('dark')
+})
